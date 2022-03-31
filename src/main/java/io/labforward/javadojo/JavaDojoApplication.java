f310f58254dab4f5f698f79a7a51388233d0c173
GilvanOrnelas <gilvan.fernandes@labforward.io>
2022-03-31 12:00:20 +0200

    Spring project

---
+++ src/main/java/io/labforward/javadojo/JavaDojoApplication.java
@@ -0,0 +1,13 @@
+package io.labforward.javadojo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDojoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaDojoApplication.class, args);
+	}
+
+}
