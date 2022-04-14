18c4d8fb5ac3d6eb07dc882c5485fd834422e70d
GilvanOrnelas <gilvan.fernandes@labforward.io>
2022-04-14 14:43:05 +0200

    User Registration Kata  description

--- src/test/java/io/labforward/javadojo/JavaDojoApplicationTests.java
+++ src/test/java/io/labforward/javadojo/JavaDojoApplicationTests.java
@@ -1,8 +1,21 @@
 package io.labforward.javadojo;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
 
+/**
+ * ======= User Registration ==========
+ * We should register new Users. Users can not be updated;
+ * The user shall provide non-empty values for: Name, email, and password;
+ * The password must have at least 6 characters;
+ * Multiple users can have the same Name, but the email must be unique.
+ * 
+ * ======= User Confirmation ==========
+ * The system shall send a confirmation email for the new user's email;
+ * The user shall confirm his account by informing his email and password;
+ * Unconfirmed users shall receive the confirmation email everyday at 9am.
+ */
 class JavaDojoApplicationTests {
 
 	@Test
